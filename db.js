36aa0989b026e22b504ed216cc51e537e672ad4b
NobreJanio <janionobrega@hotmail.com>
2024-01-11 00:01:39 -0300

    Node.js +express crud com interação mysql

---
+++ db.js
@@ -0,0 +1,37 @@
+const mysql = require("mysql2/promise");
+
+const client = mysql.createPool(process.env.CONNECTION_STRING);
+
+async function selectCustomers(){
+    const results = await client.query("SELECT * FROM tbl_clientes;");
+    return results[0];
+}
+
+async function selectCustomer(id){
+    const results = await client.query("SELECT * FROM tbl_clientes WHERE id=?;", [id]);
+    return results[0];
+}
+
+async function insertCustomer(customer){
+    const values = [customer.nome, customer.idade, customer.uf];
+    await client.query("INSERT INTO clientes(nome, idade, uf) VALUES (?,?,?)", values);
+}
+
+async function updateCustomer(id, customer){
+    const values = [customer.nome, customer.idade, customer.uf, id];
+    await client.query("UPDATE clientes SET nome=?, idade=?, uf=? WHERE id", values);
+}
+
+async function deleteCustomer(id){
+    const values = [id];
+    await client.query("DELETE FROM clientes WHERE id=?", values);
+}
+
+
+module.exports = {
+    selectCustomers,
+    selectCustomer,
+    insertCustomer,
+    updateCustomer,
+    deleteCustomer
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const db = require("./db")
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.delete("/clientes/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    await db.deleteCustomer(id);
+    response.sendStatus(204);
+})
+
+app.patch("/clientes/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    const customer = request.body;
+    db.updateCustomer(id, customer);
+    response.sendStatus(200);
+})
+
+app.post("/clientes", async (request, response) => {
+    const customer = request.body;
+    await db.insertCustomer(customer);
+    response.sendStatus(201);
+})
+
+
+app.get("/clientes/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    const results = await db.selectCustomer(id)
+    response.json(results);
+})
+
+app.get("/clientes", async (request, response) => {
+    const results = await db.selectCustomers();
+    response.json(results);
+})
+
+
+app.get("/", (request, response, next) => {
+    response.json({
+    message: "It's alive"
+    })
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log("App is running!");
+});
